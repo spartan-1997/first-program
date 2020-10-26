a0843918c077c7103b0cee47a793e8e7f976f9de
ankita080208 <52229554+ankita080208@users.noreply.github.com>
2020-10-26 20:01:16 +0530

    Update add.py
    added  comments for  explanation.

--- add.py
+++ add.py
@@ -1,3 +1,8 @@
+#taking the two variables and storing the input of integer datatype given by user in that.
+#variable1.
 a=int(raw_input())
+#variable2.
 b=int(raw_input())
+#adding the inputs and printing the result.
 print a+b
+#end of code.
